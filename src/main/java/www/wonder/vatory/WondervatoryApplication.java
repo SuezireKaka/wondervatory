0f8d722ceb369d8e6e067dfffb73fbb5cc2d8fe6
nasms018 <cjm1s@naver.com>
2023-10-04 19:12:27 +0900

    first commit

---
+++ src/main/java/www/wonder/vatory/WondervatoryApplication.java
@@ -0,0 +1,13 @@
+package www.wonder.vatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WondervatoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WondervatoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/www/wonder/vatory/WondervatoryApplicationTests.java
@@ -0,0 +1,13 @@
+package www.wonder.vatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WondervatoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
