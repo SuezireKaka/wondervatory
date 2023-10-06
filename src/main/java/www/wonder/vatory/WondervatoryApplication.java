9637504ace609ca360a4569fa39322e7fe80fa2b
SuezireKaka <rwccm3241@naver.com>
2023-10-06 15:07:08 +0900

    application 파일에 임시 주석 추가

--- src/main/java/www/wonder/vatory/WondervatoryApplication.java
+++ src/main/java/www/wonder/vatory/WondervatoryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class WondervatoryApplication {
-
+	// 연동 테스트 확인하고 지워주세요
 	public static void main(String[] args) {
 		SpringApplication.run(WondervatoryApplication.class, args);
 	}
