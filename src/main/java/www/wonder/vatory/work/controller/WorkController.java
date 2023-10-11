0765d3222257ef348e457b68fdfa21609d0668b0
SuezireKaka <rwccm3241@naver.com>
2023-10-11 17:36:02 +0900

    nsReply.xml 문제 해결

--- src/main/java/www/wonder/vatory/work/controller/WorkController.java
+++ src/main/java/www/wonder/vatory/work/controller/WorkController.java
@@ -29,14 +29,14 @@ public class WorkController {
 	private WorkService workService;
 	
 	// /work/anonymous/listAllSeries/0001/1
-	@GetMapping("/anonymous/listAllSeries/{seriesId}/{page}")
+	@GetMapping("/anonymous/listAllSeries/{boardId}/{page}")
 	public ResponseEntity<DreamPair<List<SeriesVO>, PagingDTO>> listAllSeries(@PathVariable String boardId, @PathVariable int page) {
 		DreamPair<List<SeriesVO>, PagingDTO> result = workService.listAllSeries(boardId, page);
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
 	// /work/anonymous/listAllSeries/0000/1
-	@GetMapping("/anonymous/listAllPost/{postId}/{page}")
+	@GetMapping("/anonymous/listAllPost/{seriesId}/{page}")
 	public ResponseEntity<DreamPair<List<PostVO>, PagingDTO>> listAllPost(@PathVariable String seriesId, @PathVariable int page) {
 		DreamPair<List<PostVO>, PagingDTO> result = workService.listAllPost(seriesId, page);
 		return new ResponseEntity<>(result, HttpStatus.OK);
